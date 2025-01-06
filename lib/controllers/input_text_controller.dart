661b5d22bf061d91be5dce47893043157aca0f59
David Sarri <davidsarriqsflay@gmail.com>
2025-01-06 14:24:04 +0100

    eliminadas validaciones no usadas

--- lib/controllers/input_text_controller.dart
+++ lib/controllers/input_text_controller.dart
@@ -5,16 +5,6 @@ import 'package:postal_codes_app/services/postal_code_web_service.dart';
 class InputTextController {
   final TextEditingController textController = TextEditingController();
 
-  String? validatePostalCode(String postalCode) {
-    if (postalCode.isEmpty) {
-      return 'El camp no pot estar buit.';
-    }
-    if (postalCode.length != 5 || int.tryParse(postalCode) == null) {
-      return 'El codi postal ha de tenir exactament 5 dígits.';
-    }
-    return null;
-  }
-
   Future<PostalCodeRequestModel?> fetchPostalCodeData(String postalCode) async {
     return await PostalCodeWebService.instance
         .getFromApiWithPostalCode(postalCode);
